86ce9f2633bf805f10cf0f977d3fc87bcfd6b271
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-04 22:22:15 -0500

    Added schema.sql file and Sequelize model files for Users and Entries

--- models/entry.js
+++ models/entry.js
@@ -1,7 +1,38 @@
 module.exports = function (sequelize, DataTypes) {
   const Entry = sequelize.define('Entry', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    pain_level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    strength_level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    pain_top: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    pain_bottom: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    pain_interior: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    pain_exterior: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
   });
+
+  Entry.associate = function (models) {
+    Entry.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
   return Entry;
 };
--- models/user.js
+++ models/user.js
@@ -1,7 +1,20 @@
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   });
+
+  User.associate = function (models) {
+    User.hasMany(models.Entry, {
+      onDelete: 'cascade',
+    });
+  };
+
   return User;
 };
