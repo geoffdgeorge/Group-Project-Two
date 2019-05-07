7a15a47cf64bba1205d23abf8ed902da7575251e
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-07 18:16:04 -0500

    Added a few more files and skeleton HTML to front end

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,13 @@ module.exports = function (app) {
     res.redirect('/login');
   });
 
+  app.get('/users/:username', (req, res) => {
+    const username = req.params.username;
+    db.User.findOne({ where: {username: username} }).then(results => {
+      res.send(results);
+    })
+  })
+
 //   // Load example page and pass in an example by id
 //   app.get('/example/:id', (req, res) => {
 //     db.Example.findOne({ where: { id: req.params.id } }).then((dbExample) => {
