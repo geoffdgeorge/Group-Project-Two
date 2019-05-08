bb81ff0745cf5897cb81d489619fa96e37c7dfa1
Gino DiVittorio <gino.gjd@icloud.com>
2019-05-08 16:43:35 -0500

    add test skeletons and folders

---
+++ test/entry.test.js
@@ -0,0 +1,35 @@
+const expect = require('chai').expect;
+const entry = require('../models/entry');
+
+describe('entry', function() {
+    it('pain_level should allow integer input only', function() {
+        expect(entry()).to.exist(integer);
+    });
+
+    it('strength_level should allow integer input only', function() {
+        expect(entry()).to.exist(integer);
+    });
+
+    it('pain_top should require a yes/true or no/false choice', function() {
+        expect(entry()).to.equal(boolean);
+    });
+
+    it('pain_botton should require a yes/true or no/false choice', function() {
+        expect(entry()).to.equal(boolean);
+    }); 
+
+    it('pain_interior should require a yes/true or no/false choice', function() {
+        expect(entry()).to.equal(boolean);
+    });
+
+    it('pain_exterior should require a yes/true or no/false choice', function() {
+        expect(entry()).to.equal(boolean);
+    });
+
+
+});
+
+
+// expect and equal need updated information
+// the integer and boolean may need to be replaced
+// need errors to throw
---
+++ test/user.test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect;
+const user = require('../models/user');
+
+describe('user', function() {
+    it('should contain a STRING dataType', function() {
+        expect(user()).to.equal(string);
+    });
+
+    it('should contain a STRING datatype', function() {
+        expect(user()).to.equal(string);
+    });
+
+});
+
+// need to throw errors
