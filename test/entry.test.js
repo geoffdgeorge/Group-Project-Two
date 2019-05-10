a955d84a70d7b032a2a606ce4305c04788f8df5a
Gino DiVittorio <gino.gjd@icloud.com>
2019-05-10 18:51:37 -0500

    add testing skeletons and mocha to test in package

--- test/entry.test.js
+++ test/entry.test.js
@@ -1,32 +1,46 @@
-const { expect } = require('chai');
-const entry = require('../models/entry');
+const assert = require('chai').assert;
+const entry = require('../models/entry'); //entry js link
 
-describe('entry', () => {
-  it('pain_level should allow integer input only', () => {
-    expect(entry()).to.exist(integer);
-  });
 
-  it('strength_level should allow integer input only', () => {
-    expect(entry()).to.exist(integer);
-  });
-
-  it('pain_top should require a yes/true or no/false choice', () => {
-    expect(entry()).to.equal(boolean);
-  });
-
-  it('pain_botton should require a yes/true or no/false choice', () => {
-    expect(entry()).to.equal(boolean);
-  });
-
-  it('pain_interior should require a yes/true or no/false choice', () => {
-    expect(entry()).to.equal(boolean);
-  });
-
-  it('pain_exterior should require a yes/true or no/false choice', () => {
-    expect(entry()).to.equal(boolean);
+describe('entry', function(){
+  it('pain_level should allow only an integer input of 1 to 10', function(){
+  assert.equal(entry(), integer);
   });
 });
+
+//   it('pain_level should allow integer input only', () => {
+//     expect(entry()).to.exist(integer);
+//   });
+// });
+
+
+// describe('entry', () => {
+//   it('pain_level should allow integer input only', () => {
+//     expect(entry()).to.exist(integer);
+//   });
+
+//   it('strength_level should allow integer input only', () => {
+//     expect(entry()).to.exist(integer);
+//   });
+
+//   it('pain_top should require a yes/true or no/false choice', () => {
+//     expect(entry()).to.equal(boolean);
+//   });
+
+//   it('pain_botton should require a yes/true or no/false choice', () => {
+//     expect(entry()).to.equal(boolean);
+//   });
+
+//   it('pain_interior should require a yes/true or no/false choice', () => {
+//     expect(entry()).to.equal(boolean);
+//   });
+
+//   it('pain_exterior should require a yes/true or no/false choice', () => {
+//     expect(entry()).to.equal(boolean);
+//   });
+// });
 
 // expect and equal need updated information
 // the integer and boolean may need to be replaced
 // need errors to throw
+
