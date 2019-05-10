2e7145842f8b88c361411320d93de0d988839cb5
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-10 02:14:24 -0500

    Adjusted GET call on page load so that user data prints to the entries table

--- public/js/userPage.js
+++ public/js/userPage.js
@@ -1,3 +1,5 @@
+// HTML Variables
+
 const submitForm = document.querySelector('#entry-submit');
 const strengthLevel = document.querySelector('#strength');
 const painLevel = document.querySelector('#pain');
@@ -5,6 +7,7 @@ const kneeTop = document.querySelector('#knee-top');
 const kneeBottom = document.querySelector('#knee-bottom');
 const kneeInside = document.querySelector('#knee-inside');
 const kneeOutside = document.querySelector('#knee-outside');
+const entryTable = document.querySelector('#entry-table');
 
 function handleEntrySubmit(e) {
   e.preventDefault();
@@ -23,7 +26,7 @@ function handleEntrySubmit(e) {
   axios
     .post('/api/entries', data)
     .then((response) => {
-      console.log(response);
+      location.reload();
     })
     .catch((error) => {
       console.log(error);
@@ -34,6 +37,46 @@ axios
   .get(`/api/entries/${submitForm.dataset.id}`)
   .then((response) => {
     console.log(response);
+    const entries = response.data;
+
+    entries.forEach((entry) => {
+      const entryDiv = document.createElement('div');
+      entryDiv.classList.add('entry');
+      const strengthP = document.createElement('p');
+      strengthP.textContent = entry.strength_level;
+      const painP = document.createElement('p');
+      painP.textContent = entry.pain_level;
+
+      const pT = document.createElement('p');
+      if (entry.pain_top) {
+        pT.textContent = '√';
+      }
+      const pB = document.createElement('p');
+      if (entry.pain_bottom) {
+        pB.textContent = '√';
+      }
+      const pI = document.createElement('p');
+      if (entry.pain_interior) {
+        pI.textContent = '√';
+      }
+      const pO = document.createElement('p');
+      if (entry.pain_exterior) {
+        pO.textContent = '√';
+      }
+
+      const unparsedDate = moment(entry.createdAt);
+      const date = document.createElement('p');
+      date.textContent = unparsedDate.format('MMMM Do YYYY');
+
+      entryDiv.appendChild(strengthP);
+      entryDiv.appendChild(painP);
+      entryDiv.appendChild(pT);
+      entryDiv.appendChild(pB);
+      entryDiv.appendChild(pI);
+      entryDiv.appendChild(pO);
+      entryDiv.appendChild(date);
+      entryTable.appendChild(entryDiv);
+    });
   })
   .catch((error) => {
     console.log(error);
