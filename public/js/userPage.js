9e1bf14f8d59665743ed45712d9be3018ef1336a
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-05-10 13:12:57 -0500

    Layout modified with responsive chart

--- public/js/userPage.js
+++ public/js/userPage.js
@@ -77,6 +77,31 @@ axios
       entryDiv.appendChild(date);
       entryTable.appendChild(entryDiv);
     });
+
+    // Dummy chart attached to the canvas element in userPage.hbs
+    const ctx = document.getElementById('graph').getContext('2d');
+    const chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      },
+
+      // Configuration options go here
+      options: {
+        maintainAspectRatio: false,
+      },
+    });
   })
   .catch((error) => {
     console.log(error);
