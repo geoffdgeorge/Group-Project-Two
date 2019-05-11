7a1f7639d1c1162cf7295163cea36806814fdc58
Juan Martinez <jrmarzu@gmail.com>
2019-05-11 12:45:42 -0500

    input form functioning

--- public/js/login.js
+++ public/js/login.js
@@ -1,63 +1,68 @@
-// function validateUser(user) {
-//   if (user.value.length < 6) {
-//     console.log('not enough characters');
-//     error('rgb(189,87,87)');
-//   } else {
-//     error('rgb(87,189,130)');
-//     return true;
-//   }
-// }
+function validateUser(user) {
+  if (user.value.length < 6) {
+    console.log('not enough characters');
+    error('rgb(189,87,87)');
+  } else {
+    error('rgb(87,189,130)');
+    return true;
+  }
+}
 
-// function nextSlide(parent, nextForm) {
-//   parent.classList.add('inactive');
-//   parent.classList.remove('active');
-//   nextForm.classList.add('active');
-// }
+function nextSlide(parent, nextForm) {
+  parent.classList.add('inactive');
+  parent.classList.remove('active');
+  nextForm.classList.add('active');
+}
 
-// function error(color) {
-//   document.body.style.backgroundColor = color;
-// }
+function error(color) {
+  document.body.style.backgroundColor = color;
+}
 
-// function animatedForm() {
-//   const arrows = document.querySelectorAll('.fa-arrow-down');
+function animatedForm() {
+  const arrows = document.querySelectorAll('.fa-arrow-down');
 
-//   arrows.forEach((arrow) => {
-//     arrow.addEventListener('click', () => {
-//       const input = arrow.previousElementSibling;
-//       const parent = arrow.parentElement;
-//       const nextForm = parent.nextElementSibling;
+  arrows.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+      const input = arrow.previousElementSibling;
+      const parent = arrow.parentElement;
+      const nextForm = parent.nextElementSibling;
 
-//       // Check for validation
-//       if (input.type === 'text' && validateUser(input)) {
-//         nextSlide(parent, nextForm);
-//       } else if (input.type === 'password' && validateUser(input)) {
-//         nextSlide(parent, nextForm);
-//       } else {
-//         console.log(parent);
+      // Check for validation
+      if (input.type === 'text' && validateUser(input)) {
+        nextSlide(parent, nextForm);
+      } else if (input.type === 'password' && validateUser(input)) {
+        nextSlide(parent, nextForm);
+      } else {
+        console.log(parent);
 
-//         parent.style.animation = 'shake 0.5s ease';
-//       }
-//       // get rid of animation
-//       parent.addEventListener('animationend', () => {
-//         parent.style.animation = '';
-//       });
-//     });
-//   });
-// }
+        parent.style.animation = 'shake 0.5s ease';
+      }
+      // get rid of animation
+      parent.addEventListener('animationend', () => {
+        parent.style.animation = '';
+      });
+    });
+  });
+}
 
-// animatedForm();
+animatedForm();
 
-const loginUsername = document.querySelector('#username-login');
-const loginPassword = document.querySelector('#password-login');
-const loginSubmit = document.querySelector('#submit-login');
+const loginUsername = document.getElementById('username-login');
+const loginPassword = document.getElementById('password-login');
+const loginSubmit = document.getElementById('login');
 
-function handleLogin(e) {
+loginSubmit.addEventListener('submit', function (e) {
   e.preventDefault();
+  console.log('got here');
+
+  console.log(loginSubmit.elements.value);
 
   const data = {
-    username: loginUsername.value.trim(),
-    password: loginPassword.value.trim(),
+    username: e.target.username.value.trim(),
+    password: e.target.password.value.trim(),
   };
+
+  console.log(data)
 
   // Got a hint on how to redirect axios post calls from this resource: https://stackoverflow.com/questions/49601795/making-redirects-after-an-axios-post-request-with-express
   axios
@@ -67,11 +72,10 @@ function handleLogin(e) {
         window.location = '/userPage';
       } else {
         window.location = '/login';
+        console.log(response);
       }
     })
     .catch((error) => {
       console.log(error);
     });
-}
-
-loginSubmit.addEventListener('submit', handleLogin);
+});
