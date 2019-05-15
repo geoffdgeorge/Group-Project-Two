6f524b17f13c4e83b9873f689a9b0c16f0607e2c
Gino DiVittorio <gino.gjd@icloud.com>
2019-05-15 03:23:38 -0500

    add user/entry test, delete old test

--- test/test-server.js
+++ test/test-server.js
@@ -1,48 +1,72 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server');
+const chai = require('chai'); // Require Chai assertion library
+const chaiHttp = require('chai-http'); // For integration testing for http tasks
+const express = require('express');
+const apiRoutes = require('../routes/apiRoutes.js');
 const db = require('../models');
 
-const { expect } = chai;
+const { expect } = chai; // Asset Style
+chai.use(chaiHttp); // Use chaiHTTP
 
-chai.use(chaiHttp);
-let request;
+let request; // 'let" variable so that it can be initialized later on
+
+/**
+ * This tests the APIs ability to add an entry for a given user.
+ * This test creates a new user and test if entries can be added.
+*/
 
 describe('POST /api/entries', () => {
-  // Create a new request server for testing (using test_db)
-  // & delete all examples from the db
-  beforeEach(() => {
-    request = chai.request(server);
-    return db.sequelize.sync({ force: true });
-  });
-
-  it('should save an example', (done) => {
-    // Object for endpoint testing
-    db.Test.createAll([
-      { text: 'pain_level', description: 'number between 1 and 10' },
-      { text: 'strength_level', description: 'number between 1 and 10' },
-    ]).then(() => {
-      request.get(' /api/entries').end((err, res) => {
-        const responseStatus = res.status;
-        const responseBody = res.body;
-
-        expect(err).to.be.null;
-
-        expect(responseStatus).to.equal(200); // http request will be successful
-
-        expect(responseBody[0])
-          .to.be.an('number')
-          .that.includes({ text: 'pain_level', description: 'number between 1 and 10' });
-
-        expect(responseBody[1])
-          .to.be.an('number')
-          .that.includes({ text: 'strength_level', description: 'number between 1 and 10' });
-
-        done();
-      });
+    beforeEach(async () => {
+        const app = express(); //create dummy web server
+        apiRoutes(app); // Add our route to it //
+        request = chai.request(app); // Initialize a HTTP request object for later
+        await db.sequelize.sync({ force: true }); // Sync up our test db
     });
-  });
+
+    it('should test the APIs ability to add an entry for a given user.', async () => {
+        // Object for endpoint testing
+        let id;
+        // Create a user to test. There is a constraint in the code where adding an entry must must have an existing user first
+        await db.User.create({
+            username: 'ginoworld',
+            password: '123123',
+        })
+            .then(user => {
+                console.log('JUST CREATED USER WITH ID: ', user.id);
+                id = user.id; // Save id for later (not necessary, but it will always be 1)
+            })
+            .catch(error => {
+                console.log('error creating user', error);
+            });
+
+        await db.Entry.bulkCreate([
+            {
+                "pain_level": 5,
+                "strength_level": 2,
+                "pain_top": true,
+                "pain_bottom": false,
+                "pain_exterior": false,
+                "pain_interior": false,
+                "UserId": id
+            }
+        ])
+            .then(() => {
+                request.get('/api/entries/' + id).end((err, res) => { // Makes a request
+                    const responseStatus = res.status; // Grab the status code
+                    const responseBody = res.body; // Grab the body of the response
+
+                    expect(err).to.be.null; // Expect no errors
+                    expect(responseStatus).to.equal(200); // Expect a 200 status code (successful)
+                    expect(responseBody[0]) // Expect part of the body to look like the response that this route generates
+                        .to.be.an('number')
+                        .that.includes({ text: 'pain_level', description: 'number between 1 and 10' });
+                    expect(responseBody[1])
+                        .to.be.an('number')
+                        .that.includes({ text: 'strength_level', description: 'number between 1 and 10' });
+                });
+            })
+            .catch(error => {
+                console.error('Failed to create all entries!', error); // Error will show if entries for user cannot be made
+            });
+    });
 });
 
-// Need this for initial signup, updating data
-// Test should coincide with username
