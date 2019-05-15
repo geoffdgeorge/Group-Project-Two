4ebe3d3de22426b1cbafd61fd556603be66ca772
Gino DiVittorio <gino.gjd@icloud.com>
2019-05-15 00:15:35 -0500

    remove template code and add to test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,31 +18,4 @@ module.exports = function (app) {
     });
   });
 
-  // Get all examples
-  // app.get('/api/users/:username', (req, res) => {
-  //   db.User.findAll({}).then((data) => {
-  //     res.json(data);
-  //   });
-  // });
-
-  // // Create a new user
-  // app.post('/api/users', (req, res) => {
-  //   db.User.create(req.body).then((results) => {
-  //     res.json(results);
-  //   });
-  // });
-
-  // Validate user when they attempt to sign in and redirect them to their user page
-  // app.post('/login', passport.authenticate('local'), (req, res) => {
-  //   res.redirect('/users/' + req.user.username);
-  // })
-
-  /*
-  // Delete an example by id
-  app.delete('/api/entries/:id', (req, res) => { // user should be able to delete previous entries
-    db.entry.destroy({ where: { id: req.params.id } }).then((data) => {
-      res.json(data);
-    });
-  });
-  */
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,17 +13,4 @@ module.exports = function (app) {
     });
   });
 
-  //   // Load example page and pass in an example by id
-  //   app.get('/example/:id', (req, res) => {
-  //     db.Example.findOne({ where: { id: req.params.id } }).then((dbExample) => {
-  //       res.render('example', {
-  //         example: dbExample,
-  //       });
-  //     });
-  //   });
-
-  //   // Render 404 page for any unmatched routes
-  //   app.get('*', (req, res) => {
-  //     res.render('404');
-  //   });
 };
